94c0bf1fdcce6b23ece4974bc3628e1065c84a15
Teeto <moatasem.elemam20@gmail.com>
2025-06-13 15:43:47 +0300

    updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const addButton = document.getElementById("add-task-btn");
   const taskInput = document.getElementById("task-input");
   const taskList = document.getElementById("task-list");
@@ -7,20 +7,21 @@ document.addEventListener("DOMContentLoaded", function() {
     if (taskText == "") {
       alert("Please enter a task.");
       return;
-    } else {
-      const taskItem = document.createElement("li");
-      taskItem.textContent = taskText;
-      const deleteButton = document.createElement("button");
-      deleteButton.textContent = "Remove";
-      deleteButton.className = "remove-btn";
-      deleteButton.onclick = () => {
-        taskList.removeChild(taskItem);
-      };
-      taskItem.appendChild(deleteButton);
-      taskList.appendChild(taskItem);
-      taskInput.value = "";
     }
-  };
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = taskText;
+
+    const removeButton = document.createElement("button");
+    deleteButton.textContent = "Remove";
+    deleteButton.className = "remove-btn";
+    deleteButton.onclick = () => {
+      taskList.removeChild(taskItem);
+    };
+    taskItem.appendChild(removeButton);
+    taskList.appendChild(taskItem);
+    taskInput.value = "";
+  }
   addButton.addEventListener("click", addTask);
   taskInput.addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
