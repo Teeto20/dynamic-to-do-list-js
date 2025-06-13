1127f1b49ed53474d650ad2a4074201398d5af89
Teeto <moatasem.elemam20@gmail.com>
2025-06-13 15:38:00 +0300

    updated

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
   const addButton = document.getElementById("add-task-btn");
   const taskInput = document.getElementById("task-input");
   const taskList = document.getElementById("task-list");
-  const addTask = () => {
+  function addTask() {
     const taskText = taskInput.value.trim();
     if (taskText == "") {
       alert("Please enter a task.");
